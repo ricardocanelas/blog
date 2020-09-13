d9a19847fbbcac7ab45ae5fa6f77d0a4ef787cea
Ricardo Canelas <ricardocanelas@gmail.com>
2020-09-13 18:36:55 -0300

    chore: add mdx-enhanced plugin

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
+const withPlugins = require("next-compose-plugins");
+const rehypePrism = require("@mapbox/rehype-prism");
 const withMdxEnhanced = require("next-mdx-enhanced");
 
-module.exports = withMdxEnhanced({
+// MDX-Enchanced
+const mdx = withMdxEnhanced({
   layoutPath: "layouts",
   defaultLayout: true,
   fileExtensions: ["mdx"],
   remarkPlugins: [],
-  rehypePlugins: [],
+  rehypePlugins: [rehypePrism],
   usesSrc: false,
   extendFrontMatter: {
     process: (_mdxContent, _frontMatter) => {},
     phase: "prebuild|loader|both",
   },
   reExportDataFetching: false,
-})(/* your normal nextjs config */);
+});
+
+// Next config
+const nextConfig = {
+  pageExtensions: ["js", "mdx"],
+};
+
+module.exports = withPlugins([mdx], nextConfig);
