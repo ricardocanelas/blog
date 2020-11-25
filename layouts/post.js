f4e60007bde2670a5105dbcf816afa652af2de7f
Ricardo Canelas <ricardocanelas@gmail.com>
2020-11-24 22:10:10 -0300

    feat: update post layout

--- layouts/post.js
+++ layouts/post.js
@@ -1,8 +1,21 @@
+import { format } from "date-fns";
+
+import Nav from "../components/nav";
+
 function PostLayout({ children, frontMatter }) {
+  const dateFormatted = format(new Date(frontMatter.date), "do LLLL yyyy");
+
   return (
     <div>
-      <h1>{frontMatter.title}</h1>
-      {children}
+      <Nav />
+
+      <section className="post-wrapper container mx-auto max-w-3xl p-3 md:p-0">
+        <div>
+          <div className="text-sm text-gray-600">{dateFormatted}</div>
+          <h1 className="text-5xl font-bold">{frontMatter.title}</h1>
+        </div>
+        <div className="post-content mt-6">{children}</div>
+      </section>
     </div>
   );
 }
