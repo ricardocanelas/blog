8bd4268523cdfaa0f8155f8ded7aa950b300ae1f
Ricardo Canelas <ricardocanelas@gmail.com>
2020-11-24 22:10:33 -0300

    feat: create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/index.css";
+import "prismjs/themes/prism.css";
+// import "../styles/prism-custom.css";
+// import "prismjs/themes/prism-tomorrow.css"; // dark-mode
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
